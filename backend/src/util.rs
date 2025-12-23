4852b7c9e1cf18cb7a4b77e2b973def86a360cf8
bea4dev <bea0224@outlook.jp>
2025-12-23 22:42:38 +0900

    ffmpeg command

--- backend/src/util.rs
+++ backend/src/util.rs
@@ -1,39 +1,4 @@
-use std::{env, error::Error, path::PathBuf, sync::Mutex};
-
-pub mod macros {
-    macro_rules! once {
-        ($expr:expr) => {{
-            static INIT: std::sync::Once = std::sync::Once::new();
-
-            INIT.call_once(|| {
-                $expr;
-            })
-        }};
-    }
-
-    pub(crate) use once;
-}
-
-#[derive(Debug)]
-pub struct AtomicCell<T: Copy> {
-    pub value: Mutex<T>,
-}
-
-impl<T: Copy> AtomicCell<T> {
-    pub fn new(value: T) -> Self {
-        Self {
-            value: Mutex::new(value),
-        }
-    }
-
-    pub fn get(&self) -> T {
-        *self.value.lock().unwrap()
-    }
-
-    pub fn set(&self, value: T) {
-        *self.value.lock().unwrap() = value;
-    }
-}
+use std::{env, error::Error, path::PathBuf};
 
 pub fn resolve_path_to_string(input: &str) -> Result<String, Box<dyn Error>> {
     let env_expanded = shellexpand::env(input)?; // -> Cow<str>
