0531566fbf5ed231ae0261074abafedf09f59435
aqua <aoi.kiyohara0306@gmail.com>
2026-03-07 22:56:01 +0900

    chore: add new directory for PsdCharacter

---
+++ src/lib/character/psd-character.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+type PsdCharacterProps = {
+  psd: string
+  children: React.ReactNode
+}
+
+export const PsdCharacter = ({
+    psd,
+    children
+}: PsdCharacterProps) => {
+    return null
+}
